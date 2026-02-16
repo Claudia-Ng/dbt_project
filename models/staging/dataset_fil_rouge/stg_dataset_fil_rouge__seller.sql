eeb471f72c802c4bb3768ae739d6454d3e66e72d
tchapdaprincesse@yahoo.fr <tchapdaprincesse@yahoo.fr>
2026-02-16 20:02:10 +0000

    delete

--- models/staging/dataset_fil_rouge/stg_dataset_fil_rouge__seller.sql
+++
@@ -1,5 +0,0 @@
-select seller_id,
- seller_zip_code,
- seller_city,
- seller_state
-from {{ source('dataset_fil_rouge', 'seller') }}
--- models/staging/google_sheets/stg_google_sheets__mapping_responsables_des_territoires.sql
+++
@@ -1,4 +0,0 @@
-select
-    account_manager,
-    state
-from {{ source('google_sheets', 'mapping_responsables_des_territoires') }}
