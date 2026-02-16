314ebc14c0af54f09f11a0a0bc0553fdaef295b9
tchapdaprincesse@yahoo.fr <tchapdaprincesse@yahoo.fr>
2026-02-16 19:31:53 +0000

    fix: schema

--- snapshots/local_bike/snps_local_bike__monthly_stocks.sql
+++ snapshots/local_bike/snps_local_bike__monthly_stocks.sql
@@ -1,15 +1,17 @@
 {% snapshot snps_local_bike__monthly_stocks %}
 
-{{
+{{ 
     config(
-      target_schema='snapshots',
-      unique_key='product_id',
-      strategy='check',
-      check_cols=['quantity_in_stock']
-    )
+        target_schema='snapshots',
+        unique_key='stock_id',
+        strategy='check',
+        check_cols=['all']
+    ) 
 }}
 
 SELECT
+    stock_id,
+    store_id,
     product_id,
     quantity_in_stock
 FROM {{ ref('stg_local_bike__stocks') }}
