858695898efec5be2769dfb37a926b572158d24d
Claudia-Ng <claudia.ngontcha@gmail.com>
2026-02-17 12:41:46 +0100

    Claudia ng patch 1 (#13)
    * fix: changes tests git
    
    * fix: remote files names
    
    * build: staging and int models
    
    * fix: create doc int
    
    * create mart models
    
    * delete odt files
    
    * fix: doc
    
    * fix: compile bug
    
    * fix: schema
    
    * fix:snapshot
    
    * fix:dbt_project source
    
    * fix:dbt project
    
    * fix:yml names file
    
    * fix:missing elt project.yml
    
    * fix:lineage
    
    * fix: project.yml
    
    * fix:error mrt.yml
    
    * fix:lienage and pull request changes
    
    * fix: add category and brand column on models mrt_stock
    
    * fix:mrt_stock_tracking add comma
    
    ---------
    
    Co-authored-by: tchapdaprincesse@yahoo.fr <tchapdaprincesse@yahoo.fr>

--- models/intermediate/local_bike/int_local_bike__stocks.sql
+++ models/intermediate/local_bike/int_local_bike__stocks.sql
@@ -3,10 +3,18 @@ with sales as(
   select 
   pd.product_id,
   product_name,
+  category_name,
+  brand_name,
   sum(quantity) as sales_qty
   from {{ ref('stg_local_bike__order_items' )}} oi 
   left join {{ ref('stg_local_bike__products') }} pd on pd.product_id = oi.product_id
-  group by pd.product_id, product_name
+  left join {{ ref('stg_local_bike__categories') }} ct on ct.category_id = pd.category_id
+  left join {{ ref('stg_local_bike__brands') }} br on br.brand_id = pd.brand_id
+  group by 
+  pd.product_id, 
+  product_name,
+  category_name,
+  brand_name
 ),
 stock as(
   select 
@@ -18,9 +26,17 @@ stock as(
  select 
  a.product_id,
  product_name,
+ category_name,
+ brand_name,
  COALESCE(t.stock_qty,0) as stock_qty,
  a.sales_qty
  from sales a
  left join stock t on a.product_id = t.product_id
- group by stock_qty, sales_qty, a.product_id, product_name
+ group by 
+ stock_qty, 
+ sales_qty, 
+ a.product_id, 
+ product_name,
+ category_name,
+ brand_name
 
--- models/mart/local_bike/mrt_stock_tracking.sql
+++ models/mart/local_bike/mrt_stock_tracking.sql
@@ -1,5 +1,7 @@
 select
 product_name,
+category_name,
+brand_name,
 stock_qty,
 sales_qty
 from {{ ref('int_local_bike__stocks') }} 
